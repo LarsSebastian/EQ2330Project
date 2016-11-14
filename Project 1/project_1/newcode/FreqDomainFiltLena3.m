9855dcaa552cb51ee843f4995aa5748ebcf7aa34
Jan-Zi <j-c.zim@outlook.de>
2016-11-14 12:35:07 +0100

    Add Assignment 3
    ImageRestoring for two out of focus images, FreqDomainFiltLena for
    blurring, plot of spectrum and restoration of Lena image
    used: deconvwnr(I,PSF,NSR) for restoration, maybe implement algorithm
    for wiener filter by ourselves?

---
+++ Project 1/project_1/newcode/FreqDomainFiltLena3.m
@@ -0,0 +1,90 @@
+% Assignment 2.2 for Project 1 in EQ2330
+% Fall Term 2016
+% Course EQ2330 Image and Video Processing
+% Project 1
+% Authors: Jan Zimmermann, Lars Kuger
+
+%% Clear variables and Command Window
+clc;
+clear all;
+
+%% Load Image
+
+f = imread('lena512.bmp');
+
+[M,N] = size(f);
+
+%% Image Bluring and adding noise
+
+h = myblurgen('gaussian', 8);
+noise_mean = 0;
+noise_var = 32;
+noise_gaussian = mynoisegen('gaussian', M, N, noise_mean, noise_var);
+
+f_blured = conv2(double(f), h, 'same');
+g = f_blured +noise_gaussian;
+
+
+
+%% Fast Fourier Transform 
+F = fft2(f);
+G = fft2(g);
+H = fft2(h);
+
+Fmagn = abs(F);
+Gmagn = abs(G);
+
+
+
+
+
+%% Image restoration
+ 
+nsr = noise_var/var(double(f(:)));
+f_restored = deconvwnr(g, h, nsr);
+
+for i = 1:M
+    for j = 1:N
+        f_restored(i,j) = min(max(f_restored(i,j), 0), 255);
+    end
+end
+
+
+%% Plots
+
+fig1 = figure(1);
+subplot(1,2,1);
+imshow(f);
+title('original image');
+
+subplot(1,2,2);
+imshow(uint8(g));
+title('blured and noisy image');
+
+fig2 = figure(2);
+subplot(1,2,1);
+imagesc(fftshift(log(Fmagn)));
+title('Spectrum of original image');
+
+subplot(1,2,2);
+imagesc(fftshift(log(Gmagn)));
+title('Spectrum of blured and noisy image');
+
+
+fig3 = figure(3);
+subplot(1,2,1);
+imshow(uint8(g));
+title('blured and noisy image');
+
+subplot(1,2,2);
+imshow(uint8(f_restored));
+title('restored image');
+
+
+
+
+
+
+
+
+
